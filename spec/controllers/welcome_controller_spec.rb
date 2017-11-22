e4d07f4b66aa5ab0030d6d0e6cad005cf70eedd2
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-11-22 20:47:21 +0100

    Fixed controller test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController do
-  context "get index" do
-    it "succesfully show home page" do
-      get :index
-    end
+  subject(:perform_request) { get :index }
+  before { perform_request }
+  it 'returns success response' do
+     expect(response).to be_success
   end
 end
