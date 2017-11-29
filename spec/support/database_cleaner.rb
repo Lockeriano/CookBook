31b484254cf5946cca798323f0b4585208b6b635
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-11-29 17:56:49 +0100

    Setup database_cleaner gem

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning { example.run }
+  end
+end
