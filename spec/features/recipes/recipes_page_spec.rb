1190c39feb720c6d748b859da1e589dc7111b8a6
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-11-30 17:00:32 +0100

    Made requested changes

--- spec/features/recipes/recipes_page_spec.rb
+++ spec/features/recipes/recipes_page_spec.rb
@@ -1,11 +1,18 @@
 feature 'user visits recipes page' do
   before do
     FactoryBot.create(:recipe, :scrambled_eggs)
+    FactoryBot.create(:recipe, :boiled_eggs)
+    FactoryBot.create(:recipe, :tikka_masala)
+    FactoryBot.create(:recipe, :butter_chicken)
+    FactoryBot.create(:recipe, :green_curry)
     visit('/')
   end
 
-  it 'shows user an example data' do
-    expect(page).to have_content Recipe.first.name
-    expect(page).to have_content Recipe.first.instructions
+  it 'shows recipes list' do
+    expect(page).to have_content ('scrambled eggs')
+    expect(page).to have_content ('boiled eggs')
+    expect(page).to have_content ('tikka masala')
+    expect(page).to have_content ('butter chicken')
+    expect(page).to have_content ('green curry')
   end
 end
