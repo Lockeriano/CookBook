9eeb7cb673e77a2307b933c8f6380c707b0137fa
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-12-06 07:12:26 +0100

    Made requested changes from PR and fixed issues pointed by codeclimate

--- spec/features/recipes/show_recipe_page_spec.rb
+++ spec/features/recipes/show_recipe_page_spec.rb
@@ -1,18 +1,22 @@
-feature "user visits show recipe" do
+feature 'user visits show recipe page' do
   before do
     FactoryBot.create(:recipe, :scrambled_eggs)
-    visit("/")
+    FactoryBot.create(:recipe, :tikka_masala)
+    visit('/')
   end
 
-  it "shows user clicked recipe" do
-    page.all('a')[0].click
-    have_css("img")
-    expect(page).to have_content "scrambled eggs"
-    expect(page).to have_content "egg 180 gram"
-    expect(page).to have_content "butter 5 gram"
-    expect(page).to have_content "salt 1 pinch"
-    expect(page).to have_content "pepper 1 pinch"
+  it 'shows user clicked recipe' do
+    page.all(:xpath, '//a[contains(text(), "Show")]')[0].click
+    expect(page).to have_link 'Back'
+
+    expect(page).to have_css("//img[src*='.png']")
+    expect(page).to have_content 'scrambled eggs'
+    expect(page).to have_content 'egg 180 gram'
+    expect(page).to have_content 'butter 5 gram'
+    expect(page).to have_content 'salt 1 pinch'
+    expect(page).to have_content 'pepper 1 pinch'
+
     click_on('Back')
-    expect(page).to have_current_path("/recipes")
+    expect(page).to have_current_path('/recipes')
   end
 end
