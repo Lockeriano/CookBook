875391711f390ee61f81ed45a58dacbdbb1e6b8e
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-12-07 08:02:40 +0100

    Changed rspec to test another recipe and moved page.all to before

--- spec/features/recipes/show_recipe_page_spec.rb
+++ spec/features/recipes/show_recipe_page_spec.rb
@@ -3,18 +3,17 @@ feature 'user visits show recipe page' do
     FactoryBot.create(:recipe, :scrambled_eggs)
     FactoryBot.create(:recipe, :tikka_masala)
     visit('/')
+    page.all(:xpath, '//a[contains(text(), "Show")]')[1].click
   end
 
   it 'shows user clicked recipe' do
-    page.all(:xpath, '//a[contains(text(), "Show")]')[0].click
     expect(page).to have_current_path(%r{recipes/\d+})
 
     expect(page).to have_css("//img[src*='.png']")
-    expect(page).to have_content 'scrambled eggs'
-    expect(page).to have_content 'egg 180 gram'
-    expect(page).to have_content 'butter 5 gram'
-    expect(page).to have_content 'salt 1 pinch'
-    expect(page).to have_content 'pepper 1 pinch'
+    expect(page).to have_content 'tikka masala'
+    expect(page).to have_content 'tikka a masal'
+    expect(page).to have_content 'tikka 100 mililiter'
+    expect(page).to have_content 'masala 10 gram'
 
     click_on('Back')
     expect(page).to have_current_path('/recipes')
