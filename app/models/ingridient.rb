87d1b79d1f02b25743563ffd6c8a820beb3425f3
lockeriano <patryk.skrzyniarz19@gmail.com>
2017-12-15 16:35:46 +0100

    Fixed syntax in models

--- app/models/ingridient.rb
+++ app/models/ingridient.rb
@@ -1,4 +1,4 @@
 class Ingridient < ApplicationRecord
   has_many :recipes
-  has_many :recipe_ingridients, through :recipes
+  has_many :recipe_ingridients, through: :recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   has_many :ingridients
-  has_many :recipe_ingridients
+  has_many :recipe_ingridients through: :ingridients
 end
