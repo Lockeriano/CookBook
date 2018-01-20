58b376f849f134d557e8f02f59aa17675f4a0efd
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-01-20 16:22:48 +0100

    WIP

--- app/models/recipe_ingridient.rb
+++ app/models/recipe_ingridient.rb
@@ -1,6 +1,8 @@
 class RecipeIngridient < ApplicationRecord
+  validates :unit_amount, presence: true, numericality: {greater_than: 0, less_than: 1000}
   belongs_to :recipe
   belongs_to :ingridient
 
+  accepts_nested_attributes_for :ingridient, allow_destroy: true
   delegate :name, :unit_type, to: :ingridient
 end
