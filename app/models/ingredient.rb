b85690e7624b6732cf9b9fbae2eca0e4ea918b44
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-01-29 19:49:43 +0100

    Fixed uniqueness error

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
-  validates :name, :unit_type, presence: true, uniqueness: true
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  validates :name, :unit_type, presence: true
 end
