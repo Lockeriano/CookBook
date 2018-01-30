9e7b8dd6487506c7b367777d685dca69e98e32f8
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-01-30 22:37:19 +0100

    Fixed rendering errors, added ingredients resources in routes and fixed few minor config problems with tools

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes
+  resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
-  get 'ingredients', to: 'ingredients#index'
 end
