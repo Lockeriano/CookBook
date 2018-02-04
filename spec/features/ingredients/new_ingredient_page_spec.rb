ca0ec957e1be92e50ade5db0c7946957226f1376
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-02-04 20:59:50 +0100

    rebuild tests to be more specific

--- spec/features/ingredients/new_ingredient_page_spec.rb
+++ spec/features/ingredients/new_ingredient_page_spec.rb
@@ -1,4 +1,4 @@
-feature 'user visits new ingredient page' do
+feature 'user creates new ingredient' do
   before do
     visit('/ingredients/new')
   end
@@ -8,12 +8,49 @@ feature 'user visits new ingredient page' do
     fill_in('ingredient_unit_type', with: 'Type')
     click_button('Save')
     current_path.match(ingredients_path)
+    expect(page).to have_content("Example Ingredient Type")
   end
 
-  it 'fails to create new ingredient' do
-    fill_in('ingredient_name', with: '')
-    fill_in('ingredient_unit_type', with: '')
-    click_button('Save')
-    current_path.match(new_ingredient_path)
+  context 'when empty params are submitted' do
+    before do
+      fill_in('ingredient_name', with: '')
+      fill_in('ingredient_unit_type', with: '')
+      click_button('Save')
+    end
+
+    it 'displays presence errors' do
+      current_path.match(new_ingredient_path)
+      expect(page).to have_content("can't be blank")
+    end
+  end
+
+  context 'when existing ingredient is submitted' do
+    before do
+      fill_in('ingredient_name', with: 'egg')
+      fill_in('ingredient_unit_type', with: 'gram')
+      click_button('Save')
+    end
+
+    it 'displays uniqueness error' do
+      visit('/ingredients/new')
+      fill_in('ingredient_name', with: 'egg')
+      fill_in('ingredient_unit_type', with: 'gram')
+      click_button('Save')
+      current_path.match(new_ingredient_path)
+      expect(page).to have_content('has already been taken')
+    end
+  end
+
+  context 'when too short params are submitted' do
+    before do
+      fill_in('ingredient_name', with: 'e')
+      fill_in('ingredient_unit_type', with: 'e')
+      click_button('Save')
+    end
+
+    it 'displays length error' do
+      current_path.match(new_ingredient_path)
+      expect(page).to have_content('is too short')
+    end
   end
 end
