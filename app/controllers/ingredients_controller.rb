051374dbb74416720847885b73e7fc00d9803a70
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-02-07 23:18:04 +0100

    recovering from the git fiasco

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,38 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:succes] = 'Ingredient created sucessfully'
+      redirect_to ingredients_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(ingredient_params)
+      flash[:succes] = 'Ingredient updated sucessfully'
+      redirect_to ingredients_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :unit_type)
+  end
 end
