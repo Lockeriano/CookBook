a7e385fca7a47f4a6865f9000ea5982ac7519f48
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-03-14 21:40:21 +0100

    Minor semantic fixes

--- spec/features/ingredients/delete_ingredient_page_spec.rb
+++ spec/features/ingredients/delete_ingredient_page_spec.rb
@@ -1,14 +1,17 @@
-feature 'user visits ingredients page' do
+feature 'user deletes ingredient' do
   before do
     FactoryBot.create(:ingredient, :egg)
+    FactoryBot.create(:ingredient, :salt)
     visit('/ingredients')
-    click_link('Delete')
+    page.all(:xpath, '//a[contains(text(), "Delete")]')[1].click
   end
 
   it 'successfully delete ingredient' do
     current_path.match(ingredients_path)
     expect(page).to have_content('Ingredient was deleted')
-    expect(page).to have_no_content('egg')
-    expect(page).to have_no_content('gram')
+    expect(page).to have_content('egg')
+    expect(page).to have_content('gram')
+    expect(page).to have_no_content('salt')
+    expect(page).to have_no_content('pinch')
   end
 end
