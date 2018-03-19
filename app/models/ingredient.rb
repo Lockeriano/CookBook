17d017af10eaeeb4193632814f7147f26f1daa24
Patryk Skrzyniarz <patryk.skrzyniarz19@gmail.com>
2018-03-19 17:06:51 +0100

    Delete ingredient (#18)

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   validates :name, :unit_type, presence: true, length: {minimum: 2}
   validates_uniqueness_of :name
 
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients
 end
