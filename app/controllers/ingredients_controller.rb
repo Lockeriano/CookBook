704b93867fbb3c6d09f122fb96e601a05f249f65
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-03-19 17:00:20 +0100

    Recovering from the git fiasco

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,47 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = 'Ingredient created successfully'
+      redirect_to ingredients_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(ingredient_params)
+      flash[:success] = 'Ingredient updated successfully'
+      redirect_to ingredients_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.destroy
+      flash[:notice] = 'Ingredient was deleted'
+      redirect_to ingredients_path
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :unit_type)
+  end
+end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ApplicationRecord
-  has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients
+  validates :name, :unit_type, presence: true, length: {minimum: 2}
+  validates_uniqueness_of :name
 
-  validates :name, :unit_type, presence: true
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
 end
