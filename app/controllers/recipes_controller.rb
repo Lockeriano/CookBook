c0357ea4aec80f256c304803ca228ced9ea9b659
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-06-05 15:46:31 +0200

    Create delete button for each recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 class RecipesController < ApplicationController
+  before_action :find_recipe, except: [:index, :new, :create]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -24,11 +25,9 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:success] = 'Recipe updated successfully'
       redirect_to recipe_path
@@ -37,9 +36,20 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    if @recipe.destroy
+      flash[:success] = 'Recipe deleted successfully'
+      redirect_to recipes_path
+    end
+  end
+
   private
 
   def recipe_params
     params.require(:recipe).permit(:id, :name, :instructions, recipe_ingredients_attributes: [:id, :ingredient_id, :recipe_id, :unit_amount, :_destroy])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
