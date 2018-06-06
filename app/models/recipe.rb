c532e278daadd6d91803a9f1216b84aeb68d4a15
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-06-06 11:38:27 +0200

    Fix CI issues

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  accepts_nested_attributes_for :recipe_ingredients, reject_if: proc { |attribute| attribute['unit_amount'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :recipe_ingredients,
+    reject_if: proc { |attribute| attribute['unit_amount'].blank? }, allow_destroy: true
+
   validates_associated :recipe_ingredients
 end
