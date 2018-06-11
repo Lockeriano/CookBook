866373e4171b230bd150f8f495a7ba577b211941
lockeriano <patryk.skrzyniarz19@gmail.com>
2018-06-11 11:51:16 +0200

    Allows user to add recipe image of his choice, add validation to images and few tests to check it

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -41,7 +41,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:id, :name, :instructions, recipe_ingredients_attributes:
-                                                              %i(id ingredient_id recipe_id unit_amount _destroy))
+    params.require(:recipe).permit(:id, :name, :instructions, :image,
+      recipe_ingredients_attributes: %i(id ingredient_id recipe_id unit_amount _destroy))
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,11 @@
 class Recipe < ApplicationRecord
   validates :name, :instructions, presence: true
   validates :instructions, length: {minimum: 10}
+  has_attached_file :image, styles: {thumbnail: '250x250#'}, default_url: '/assets/:style/missing.jpg'
+
+  validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\z}
+  validates_attachment_size :image, less_than: 5.megabytes
+  validate :check_dimensions
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
@@ -11,4 +16,19 @@ class Recipe < ApplicationRecord
     reject_if: proc { |attribute| attribute['unit_amount'].blank? }, allow_destroy: true
 
   validates_associated :recipe_ingredients
+
+  private
+
+  def check_dimensions
+    temp_file = image.queued_for_write[:original]
+    unless temp_file.nil?
+      dimensions = Paperclip::Geometry.from_file(temp_file)
+      width = dimensions.width
+      height = dimensions.height
+
+      if width < 250 && height < 250
+        errors[:image] << ' minimum width: 250px, minimum height: 250px'
+      end
+    end
+  end
 end
